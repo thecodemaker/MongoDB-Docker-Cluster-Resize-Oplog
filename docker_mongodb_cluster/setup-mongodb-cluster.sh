fc1f64ff3b692db2c0f1b78734fa218092da871b
Bogdan Apetrei <bogdan.apetrei@endava.com>
2015-12-09 14:21:01 +0200

    refactor docker files

--- docker_mongodb_cluster/setup-mongodb-cluster.sh
+++ docker_mongodb_cluster/setup-mongodb-cluster.sh
@@ -24,9 +24,17 @@ docker run -d -v /var/run/docker.sock:/docker.sock --name skydock crosbymichael/
 sleep 2
 
 echo "*******************************************Create mongodocker images"
-docker build -t dev0/mongodb mongod
-docker build -t dev0/mongos mongos
-docker build -t dev0/mongoconfig mongoconfig
+echo "build dev0/mongobase"
+docker build --no-cache=true -t dev0/mongobase mongobase
+
+echo "build dev0/mongodb"
+docker build --no-cache=true -t dev0/mongodb mongod
+
+echo "build dev0/mongoconfig"
+docker build --no-cache=true -t dev0/mongoconfig mongoconfig
+
+echo "build dev0/mongos"
+docker build --no-cache=true -t dev0/mongos mongos
 
 echo "************************************************Create a replica set"
 for i in {1..3}
@@ -82,7 +90,7 @@ docker run \
     --dns ${DOCKERIP} \
     --name mongos1 \
     -P -i -d dev0/mongos \
-    --configdb "cfg1.mongodb.dev.docker:27017" \
+    --configdb "cfg1.mongoconfig.dev.docker:27017" \
     --port 27017
 
 sleep 10
@@ -116,76 +124,6 @@ do
     sleep 5
 done
 
-
-
-
-#echo "*******************************Create index on secondary replication"
-#echo "**********************************************Put down one secondary"
-#
-#docker stop -t=10 rs1_srv2
-#
-#sleep 5
-#
-#mongo --port $(docker port rs1_srv1 27017|cut -d ":" -f2) <<EOF
-#    rs.status()
-#EOF
-#
-#echo "***********************************Create new secondary on same data"
-#
-#docker run \
-#     --dns ${DOCKERIP}  \
-#     --name rs1_srv2_bis \
-#     -v "`pwd`/mongo-persisted-data/rs1_srv2":/data/mongodb \
-#     -P -i -d dev0/mongodb \
-#     --dbpath /data/mongodb --noprealloc --smallfiles --profile=0 --slowms=-1
-#
-#sleep 5
-#
-#docker ps -a
-#
-#mongo --port $(docker port rs1_srv2_bis 27017|cut -d ":" -f2) < js/addIndexOnlyOnSecondary.js
-#
-#sleep 5
-#
-#echo "***********************************************Restart old secondary"
-#
-#docker stop -t=10 rs1_srv2_bis
-#
-#docker start rs1_srv2
-#
-#sleep 5
-#
-#mongo --port $(docker port rs1_srv1 27017|cut -d ":" -f2) <<EOF
-#    rs.status()
-#EOF
-#
-#echo "*******************************************************Execute query"
-#
-#mongo --port $(docker port rs1_srv2 27017|cut -d ":" -f2) <<EOF
-#    rs.slaveOk();
-#    use test_database;
-#
-#    printjson(db.test_collections.find({randomValue: {\$gt: 0}, createdDtm: {\$gt: 0}}).limit(1).explain(true));
-#EOF
-#
-#echo "***********************************************Populate initial data"
-#
-#mongo --port $(docker port mongos1 27017|cut -d ":" -f2) < js/populateDatabase.js
-#
-#echo "**********************************************Check data replication"
-#
-#mongo --port $(docker port rs1_srv1 27017|cut -d ":" -f2) < js/checkNumberOfDocuments.js
-#
-#sleep 5
-#
-#mongo --port $(docker port rs1_srv2 27017|cut -d ":" -f2) < js/checkNumberOfDocuments.js
-#
-#sleep 5
-#
-#mongo --port $(docker port rs1_srv3 27017|cut -d ":" -f2) < js/checkNumberOfDocuments.js
-#
-#
-###
 ####sudo docker logs rs1_srv1
 ####sudo docker logs rs1_srv2
 ####sudo docker logs rs1_srv3
